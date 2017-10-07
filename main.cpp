72173b37b43df9e8c60662bc169fe604c6d6deb4
张晓民 <zhang_xiao_min@foxmail.com>
2017-10-08 03:20:49 +0800

    MedicalImage 20171008

--- main.cpp
+++ main.cpp
@@ -1,36 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
 
-#include<iostream>
-#include<string>
-#include<vector>
-using namespace std;
-
-#include <opencv2/core/core.hpp>
-#include<opencv2/highgui/highgui.hpp>
-#include<opencv2/imgproc/imgproc.hpp>
-using namespace cv;
-
 int main(int argc, char *argv[])
 {
-    Mat image=imread ("/Users/zhangxiaomin/Desktop/111.jpg");
-    if(image.empty ())
-    {
-        cerr<<"文件路径错误!";
-        return -1;
-    }
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
 
-    namedWindow ("src",WINDOW_AUTOSIZE);
-    imshow ("src",image);
-    waitKey (0);
-    destroyWindow ("src");
-    return 0;
-
-
-
-//    QApplication a(argc, argv);
-//    MainWindow w;
-//    w.show();
-
-//    return a.exec();
+    return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include<QImage>
+#include<QPixmap>
+#include<QMessageBox>
+#include<QFileDialog>
+#include<QTextCodec>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +16,640 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+/**
+ * @brief MainWindow::setLablePixmapWithMat:使用openCV的图像来设置lable，缩放
+ * @param lable
+ * @param image
+ */
+void MainWindow::setLablePixmapWithMat (QLabel *lable,Mat image)
+{
+    QImage img=QImage((const unsigned char*)(image.data),
+                      image.cols,
+                      image.rows,
+                      image.cols*image.channels (),
+                      QImage::Format_RGB888);
+    img=img.scaled (lable->size ());
+    lable->clear ();
+    lable->setPixmap (QPixmap::fromImage (img));
+}
+
+/**
+ * @brief MainWindow::setLablePixmapWithMatNoScale:使用openCV的图像来设置lable，不缩放
+ * @param lable
+ * @param image
+ */
+void MainWindow::setLablePixmapWithMatNoScale (QLabel *lable,Mat image)
+{
+    QImage img=QImage((const unsigned char*)(image.data),
+                      image.cols,
+                      image.rows,
+                      image.cols*image.channels (),
+                      QImage::Format_RGB888);
+    lable->clear ();
+    lable->setPixmap (QPixmap::fromImage (img));
+}
+
+/**
+ * @brief MainWindow::showWarningMessageBox:显示提示框
+ * @param title
+ * @param text
+ */
+void MainWindow::showWarningMessageBox(QString title,QString text)
+{
+    QMessageBox msgBox;
+    msgBox.setWindowTitle (title);
+    msgBox.setText (text);
+    msgBox.exec ();
+}
+
+/**
+ * @brief MainWindow::selectMatchTemplate:选择一个模板
+ * @return
+ */
+Mat MainWindow::selectMatchTemplate ()
+{
+     Mat tempImage;
+    QString fileName=QFileDialog::getOpenFileName (this,tr("选择测试文件"),".",tr("Image Files(*.bmp *.jpg *.jpeg *.png)"));
+    if(!fileName.isEmpty ())//读到文件了
+    {
+        QTextCodec *codec=QTextCodec::codecForName ("gb18030");
+        std::string name=codec->fromUnicode (fileName).data ();
+        tempImage=imread (name);
+        if(tempImage.data)
+        {
+            cvtColor (tempImage,tempImage,CV_BGR2RGB);
+        }
+        else
+        {
+            this->showWarningMessageBox (tr("错误"),tr("OpenCV读取文件不成功"));
+        }
+    }
+    else //没有读到文件
+    {
+        this->showWarningMessageBox (tr("测试文件不存在"),tr("所选择的测试文件不存在，或者文件已损坏无法读取"));
+    }
+    return tempImage;
+}
+
+
+/**
+ * @brief MainWindow::on_action_Open_TestImage_triggered：打开测试文件
+ */
+void MainWindow::on_action_Open_TestImage_triggered()
+{
+    QString fileName=QFileDialog::getOpenFileName (this,tr("选择测试文件"),".",tr("Image Files(*.bmp *.jpg *.jpeg *.png)"));
+    if(!fileName.isEmpty ())//读到文件了
+    {
+        QTextCodec *codec=QTextCodec::codecForName ("gb18030");
+        std::string name=codec->fromUnicode (fileName).data ();
+        this->testImage=imread (name);
+        if(this->testImage.data)
+        {
+            cvtColor (this->testImage,this->testImage,CV_BGR2RGB);
+            this->setLablePixmapWithMat (ui->testImage,this->testImage);
+        }
+        else
+        {
+            this->showWarningMessageBox (tr("错误"),tr("OpenCV读取文件不成功"));
+        }
+    }
+    else //没有读到文件
+    {
+        this->showWarningMessageBox (tr("测试文件不存在"),tr("所选择的测试文件不存在，或者文件已损坏无法读取"));
+    }
+}
+
+/**
+ * @brief MainWindow::on_action_Restore_triggered:还原
+ */
+void MainWindow::on_action_Restore_triggered()
+{
+    this->testImage.copyTo (this->destImage);
+    this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+}
+
+/**
+ * @brief MainWindow::on_actionClear_triggered:清除
+ */
+void MainWindow::on_actionClear_triggered()
+{
+    ui->testImage->clear ();
+    ui->dstImage->clear ();
+}
+
+/**
+ * @brief MainWindow::on_actionExit_triggered:退出程序
+ */
+void MainWindow::on_actionExit_triggered()
+{
+    qApp->exit (0);
+}
+
+/**
+ * @brief MainWindow::on_actionHistEqualize_triggered:直方图均衡化
+ */
+void MainWindow::on_actionHistEqualize_triggered()
+{
+    if(this->testImage.data)//已经有数据了
+    {
+        //定义一个数组，用于存储各通道的向量
+        vector<Mat> splitBGR(this->testImage.channels ());
+        //分割通道
+        split (this->testImage,splitBGR);
+        //对各个通道分别进行直方图均衡化
+        for(int i=0;i<this->testImage.channels ();i++)
+        {
+            equalizeHist (splitBGR[i],splitBGR[i]);
+        }
+
+        //合并图像
+        Mat mergeImg;
+        merge(splitBGR,mergeImg);
+        // 复制图像
+        mergeImg.copyTo (this->destImage);
+        this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+    }
+    else
+    {
+        this->showWarningMessageBox (tr("错误!"),tr("当前尚未选择测试文件！"));
+    }
+}
+
+/**
+ * @brief MainWindow::on_actionContrast_triggered:调节对比度
+ */
+void MainWindow::on_actionContrast_triggered()
+{
+    if(this->testImage.data)//已经有数据了
+    {
+        this->testImage.convertTo (this->destImage,-1,2);
+        this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+    }
+    else
+    {
+        this->showWarningMessageBox (tr("错误!"),tr("当前尚未选择测试文件！"));
+    }
+}
+
+/**
+ * @brief MainWindow::on_actionLight_triggered:亮度
+ */
+void MainWindow::on_actionLight_triggered()
+{
+    if(this->testImage.data)//已经有数据了
+    {
+        this->testImage.convertTo (this->destImage,-1,1,80);
+        this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+    }
+    else
+    {
+        this->showWarningMessageBox (tr("错误!"),tr("当前尚未选择测试文件！"));
+    }
+}
+
+/**
+ * @brief MainWindow::on_actionContrastAndLight_triggered:对比度和亮度
+ */
+void MainWindow::on_actionContrastAndLight_triggered()
+{
+    if(this->testImage.data)//已经有数据了
+    {
+        this->testImage.convertTo (this->destImage,-1,2,80);
+        this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+    }
+    else
+    {
+        this->showWarningMessageBox (tr("错误!"),tr("当前尚未选择测试文件！"));
+    }
+}
+
+/**
+ * @brief MainWindow::on_action_filter2D1_triggered：卷积，算子1
+ */
+void MainWindow::on_action_filter2D1_triggered()
+{
+    if(this->testImage.data)//已经有数据了
+    {
+        //算子1
+        //建立核
+        Mat kernel=(Mat_<float>(3,3)<<0,-1,0,-1,5,-1,0,-1,0);
+        filter2D (this->testImage,this->destImage,this->testImage.depth (),kernel);
+
+        this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+    }
+    else
+    {
+        this->showWarningMessageBox (tr("错误!"),tr("当前尚未选择测试文件！"));
+    }
+}
+
+/**
+ * @brief MainWindow::on_action_filter2D2_triggered:卷积，算子2
+ */
+void MainWindow::on_action_filter2D2_triggered()
+{
+    if(this->testImage.data)//已经有数据了
+    {
+        //算子2
+        //建立核
+        Mat kernel=Mat(3,3,CV_32F,Scalar(-1));
+        //分配像素值
+        kernel.at<float>(1,1)=8;
+        filter2D (this->testImage,this->destImage,this->testImage.depth (),kernel);
+
+        this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+    }
+    else
+    {
+        this->showWarningMessageBox (tr("错误!"),tr("当前尚未选择测试文件！"));
+    }
+}
+
+/**
+ * @brief MainWindow::on_action_filter2D3_triggered:卷积，算子3
+ */
+void MainWindow::on_action_filter2D3_triggered()
+{
+    if(this->testImage.data)//已经有数据了
+    {
+        //算子3
+        //建立核
+        Mat kernel=Mat(3,3,CV_32F,Scalar(-1));
+        //分配像素值
+        kernel.at<float>(1,1)=8.9f;
+        filter2D (this->testImage,this->destImage,this->testImage.depth (),kernel);
+
+        this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+    }
+    else
+    {
+        this->showWarningMessageBox (tr("错误!"),tr("当前尚未选择测试文件！"));
+    }
+}
+
+/**
+ * @brief MainWindow::on_action_filter2D4_triggered:卷积，算子4
+ */
+void MainWindow::on_action_filter2D4_triggered()
+{
+    if(this->testImage.data)//已经有数据了
+    {
+        //算子4
+        //建立核
+        Mat kernel=Mat(3,3,CV_32F,Scalar(0));
+        //分配像素值
+        kernel.at<float>(1,1)=5.0;
+        kernel.at<float>(0,1)=-1.0;
+        kernel.at<float>(2,1)=-1.0;
+
+        filter2D (this->testImage,this->destImage,this->testImage.depth (),kernel);
+
+        this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+    }
+    else
+    {
+        this->showWarningMessageBox (tr("错误!"),tr("当前尚未选择测试文件！"));
+    }
+}
+
+/**
+ * @brief MainWindow::on_action_filter2D5_triggered:卷积，算子5
+ */
+void MainWindow::on_action_filter2D5_triggered()
+{
+    if(this->testImage.data)//已经有数据了
+    {
+        //算子5
+        //建立核
+        Mat kernel=Mat(3,3,CV_32F,Scalar(0));
+        //分配像素值
+        kernel.at<float>(1,1)=5.0;
+        kernel.at<float>(1,0)=-1.0;
+        kernel.at<float>(1,2)=-1.0;
+
+        filter2D (this->testImage,this->destImage,this->testImage.depth (),kernel);
+
+        this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+    }
+    else
+    {
+        this->showWarningMessageBox (tr("错误!"),tr("当前尚未选择测试文件！"));
+    }
+}
+
+/**
+ * @brief MainWindow::on_action_filter2D6_triggered:卷积，算子6
+ */
+void MainWindow::on_action_filter2D6_triggered()
+{
+    if(this->testImage.data)//已经有数据了
+    {
+        //算子6
+        //建立核
+        Mat kernel=(Mat_<float>(3,3)<<0,-1,0,-1,6,-1,0,-1,0);
+
+        filter2D (this->testImage,this->destImage,this->testImage.depth (),kernel);
+
+        this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+    }
+    else
+    {
+        this->showWarningMessageBox (tr("错误!"),tr("当前尚未选择测试文件！"));
+    }
+}
+
+/**
+ * @brief MainWindow::on_actionTM_SQDIFF_triggered:模板匹配，TM_SQDIFF
+ */
+void MainWindow::on_actionTM_SQDIFF_triggered()
+{
+    //TM_SQDIFF模板匹配效果
+    Mat templ;
+    Mat result;
+    int match_method=TM_SQDIFF;
+
+    templ=this->selectMatchTemplate ();
+    if(templ.data)
+    {
+        Mat img_display;
+        this->testImage.copyTo (img_display);
+
+        int result_cols=this->testImage.cols-templ.cols+1;
+        int result_rows=this->testImage.rows-templ.rows+1;
+
+        result.create (result_cols,result_rows,CV_32FC1);
+        matchTemplate (this->testImage,templ,result,match_method);
+        normalize(result,result,0,1,NORM_MINMAX,-1,Mat());
+
+        //使用minMaxLoc对匹配结果进行定位
+        double minVal;
+        double maxVal;
+        Point minLoc;
+        Point maxLoc;
+        Point matchLoc;
+        minMaxLoc (result,&minVal,&maxVal,&minLoc,&maxLoc,Mat());
+        if(match_method==TM_SQDIFF||match_method==TM_SQDIFF_NORMED)
+            matchLoc=minLoc;
+        else
+            matchLoc=maxLoc;
+
+        //处理结果
+        rectangle (img_display,matchLoc,Point(matchLoc.x+templ.cols,matchLoc.y+templ.rows),
+                   Scalar::all (0),2,8,0);
+        rectangle (result,matchLoc,Point(matchLoc.x+templ.cols,matchLoc.y+templ.rows),
+                           Scalar::all (255),2,8,0);
+        img_display.copyTo (this->destImage);
+        this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+    }
+    else
+    {
+        this->showWarningMessageBox (tr("错误"),tr("选择的模板文件无效！"));
+    }
+}
+
+/**
+ * @brief MainWindow::on_actionTM_SQDIFF_NORMED_triggered:TM_SQDIFF_NORMED
+ */
+void MainWindow::on_actionTM_SQDIFF_NORMED_triggered()
+{
+    //TM_SQDIFF_NORMED模板匹配效果
+    Mat templ;
+    Mat result;
+    int match_method=TM_SQDIFF_NORMED;
+
+    templ=this->selectMatchTemplate ();
+    if(templ.data)
+    {
+        Mat img_display;
+        this->testImage.copyTo (img_display);
+
+        int result_cols=this->testImage.cols-templ.cols+1;
+        int result_rows=this->testImage.rows-templ.rows+1;
+
+        result.create (result_cols,result_rows,CV_32FC1);
+        matchTemplate (this->testImage,templ,result,match_method);
+        normalize(result,result,0,1,NORM_MINMAX,-1,Mat());
+
+        //使用minMaxLoc对匹配结果进行定位
+        double minVal;
+        double maxVal;
+        Point minLoc;
+        Point maxLoc;
+        Point matchLoc;
+        minMaxLoc (result,&minVal,&maxVal,&minLoc,&maxLoc,Mat());
+        if(match_method==TM_SQDIFF||match_method==TM_SQDIFF_NORMED)
+            matchLoc=minLoc;
+        else
+            matchLoc=maxLoc;
+
+        //处理结果
+        rectangle (img_display,matchLoc,Point(matchLoc.x+templ.cols,matchLoc.y+templ.rows),
+                   Scalar::all (0),2,8,0);
+        rectangle (result,matchLoc,Point(matchLoc.x+templ.cols,matchLoc.y+templ.rows),
+                           Scalar::all (255),2,8,0);
+        img_display.copyTo (this->destImage);
+        this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+    }
+    else
+    {
+        this->showWarningMessageBox (tr("错误"),tr("选择的模板文件无效！"));
+    }
+}
+
+/**
+ * @brief MainWindow::on_actionTM_CCORR_triggered:TM_CCORR
+ */
+void MainWindow::on_actionTM_CCORR_triggered()
+{
+    //TM_CCORR模板匹配效果
+    Mat templ;
+    Mat result;
+    int match_method=TM_CCORR;
+
+    templ=this->selectMatchTemplate ();
+    if(templ.data)
+    {
+        Mat img_display;
+        this->testImage.copyTo (img_display);
+
+        int result_cols=this->testImage.cols-templ.cols+1;
+        int result_rows=this->testImage.rows-templ.rows+1;
+
+        result.create (result_cols,result_rows,CV_32FC1);
+        matchTemplate (this->testImage,templ,result,match_method);
+//        normalize(result,result,0,1,NORM_MINMAX,-1,Mat());
+
+        //使用minMaxLoc对匹配结果进行定位
+        double minVal;
+        double maxVal;
+        Point minLoc;
+        Point maxLoc;
+        Point matchLoc;
+        minMaxLoc (result,&minVal,&maxVal,&minLoc,&maxLoc,Mat());
+        if(match_method==TM_SQDIFF||match_method==TM_SQDIFF_NORMED)
+            matchLoc=minLoc;
+        else
+            matchLoc=maxLoc;
+
+        //处理结果
+        rectangle (img_display,matchLoc,Point(matchLoc.x+templ.cols,matchLoc.y+templ.rows),
+                   Scalar::all (0),2,8,0);
+//        rectangle (result,matchLoc,Point(matchLoc.x+templ.cols,matchLoc.y+templ.rows),
+//                           Scalar::all (255),2,8,0);
+        img_display.copyTo (this->destImage);
+        this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+    }
+    else
+    {
+        this->showWarningMessageBox (tr("错误"),tr("选择的模板文件无效！"));
+    }
+}
+
+/**
+ * @brief MainWindow::on_actionTM_CCORR_NORMED_triggered:TM_CCORR_NORMED
+ */
+void MainWindow::on_actionTM_CCORR_NORMED_triggered()
+{
+    //TM_CCORR_NORMED模板匹配效果
+    Mat templ;
+    Mat result;
+    int match_method=TM_CCORR_NORMED;
+
+    templ=this->selectMatchTemplate ();
+    if(templ.data)
+    {
+        Mat img_display;
+        this->testImage.copyTo (img_display);
+
+        int result_cols=this->testImage.cols-templ.cols+1;
+        int result_rows=this->testImage.rows-templ.rows+1;
+
+        result.create (result_cols,result_rows,CV_32FC1);
+        matchTemplate (this->testImage,templ,result,match_method);
+        normalize(result,result,0,1,NORM_MINMAX,-1,Mat());
+
+        //使用minMaxLoc对匹配结果进行定位
+        double minVal;
+        double maxVal;
+        Point minLoc;
+        Point maxLoc;
+        Point matchLoc;
+        minMaxLoc (result,&minVal,&maxVal,&minLoc,&maxLoc,Mat());
+        if(match_method==TM_SQDIFF||match_method==TM_SQDIFF_NORMED)
+            matchLoc=minLoc;
+        else
+            matchLoc=maxLoc;
+
+        //处理结果
+        rectangle (img_display,matchLoc,Point(matchLoc.x+templ.cols,matchLoc.y+templ.rows),
+                   Scalar::all (0),2,8,0);
+        rectangle (result,matchLoc,Point(matchLoc.x+templ.cols,matchLoc.y+templ.rows),
+                           Scalar::all (255),2,8,0);
+        img_display.copyTo (this->destImage);
+        this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+    }
+    else
+    {
+        this->showWarningMessageBox (tr("错误"),tr("选择的模板文件无效！"));
+    }
+}
+
+/**
+ * @brief MainWindow::on_actionTM_CCOFF_triggered
+ */
+void MainWindow::on_actionTM_CCOFF_triggered()
+{
+    //TM_CCOEFF_NORMED模板匹配效果
+    Mat templ;
+    Mat result;
+    int match_method=TM_CCOEFF;
+
+    templ=this->selectMatchTemplate ();
+    if(templ.data)
+    {
+        Mat img_display;
+        this->testImage.copyTo (img_display);
+
+        int result_cols=this->testImage.cols-templ.cols+1;
+        int result_rows=this->testImage.rows-templ.rows+1;
+
+        result.create (result_cols,result_rows,CV_32FC1);
+        matchTemplate (this->testImage,templ,result,match_method);
+        normalize(result,result,0,1,NORM_MINMAX,-1,Mat());
+
+        //使用minMaxLoc对匹配结果进行定位
+        double minVal;
+        double maxVal;
+        Point minLoc;
+        Point maxLoc;
+        Point matchLoc;
+        minMaxLoc (result,&minVal,&maxVal,&minLoc,&maxLoc,Mat());
+        if(match_method==TM_SQDIFF||match_method==TM_SQDIFF_NORMED)
+            matchLoc=minLoc;
+        else
+            matchLoc=maxLoc;
+
+        //处理结果
+        rectangle (img_display,matchLoc,Point(matchLoc.x+templ.cols,matchLoc.y+templ.rows),
+                   Scalar::all (0),2,8,0);
+        rectangle (result,matchLoc,Point(matchLoc.x+templ.cols,matchLoc.y+templ.rows),
+                           Scalar::all (255),2,8,0);
+        img_display.copyTo (this->destImage);
+        this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+    }
+    else
+    {
+        this->showWarningMessageBox (tr("错误"),tr("选择的模板文件无效！"));
+    }
+}
+
+/**
+ * @brief MainWindow::on_actionTM_CCOEFF_NORMED_triggered
+ */
+void MainWindow::on_actionTM_CCOEFF_NORMED_triggered()
+{
+    //TM_CCOEFF_NORMED模板匹配效果
+    Mat templ;
+    Mat result;
+    int match_method=TM_CCOEFF_NORMED;
+
+    templ=this->selectMatchTemplate ();
+    if(templ.data)
+    {
+        Mat img_display;
+        this->testImage.copyTo (img_display);
+
+        int result_cols=this->testImage.cols-templ.cols+1;
+        int result_rows=this->testImage.rows-templ.rows+1;
+
+        result.create (result_cols,result_rows,CV_32FC1);
+        matchTemplate (this->testImage,templ,result,match_method);
+        normalize(result,result,0,1,NORM_MINMAX,-1,Mat());
+
+        //使用minMaxLoc对匹配结果进行定位
+        double minVal;
+        double maxVal;
+        Point minLoc;
+        Point maxLoc;
+        Point matchLoc;
+        minMaxLoc (result,&minVal,&maxVal,&minLoc,&maxLoc,Mat());
+        if(match_method==TM_SQDIFF||match_method==TM_SQDIFF_NORMED)
+            matchLoc=minLoc;
+        else
+            matchLoc=maxLoc;
+
+        //处理结果
+        rectangle (img_display,matchLoc,Point(matchLoc.x+templ.cols,matchLoc.y+templ.rows),
+                   Scalar::all (0),2,8,0);
+        rectangle (result,matchLoc,Point(matchLoc.x+templ.cols,matchLoc.y+templ.rows),
+                           Scalar::all (255),2,8,0);
+        img_display.copyTo (this->destImage);
+        this->setLablePixmapWithMat (ui->dstImage,this->destImage);
+    }
+    else
+    {
+        this->showWarningMessageBox (tr("错误"),tr("选择的模板文件无效！"));
+    }
 }
